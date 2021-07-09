c8ad9ea2f4801c2cd00e6ed57becb33e9d8bb5ff
JMR-Mejia <chucho4720013@gmail.com>
2021-07-09 13:56:32 -0500

    type error in config

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const config = {
   dev: process.env.NODE_ENV !== "production",
   port: process.env.PORT || 3000,
   cors: process.env.CORS,
-  pgHost: process.env.PG_HOST || "localhost",
+  pgHost: process.env.PGHOST || "localhost",
   pgUser: process.env.PGUSER,
   pgDatabase: process.env.PGDATABASE,
   pgPassword: process.env.PGPASSWORD,
